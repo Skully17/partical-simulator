f36a47fb8281f0a78ed54217ad1c34eecbe5cd97
skully17 <skully1717@gmail.com>
2025-07-03 08:45:31 +0100

    - Adding fundamentals for GUI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,200 @@
 use std::thread::sleep;
 
+// This module integrations this application with the graphics engine.
+
+use std::f32::consts::TAU;
+
+use graphics::{
+    Camera, ControlScheme, DeviceEvent, EngineUpdates, Entity, InputSettings, LightType, Lighting,
+    Mesh, PointLight, Scene, UiLayout, UiSettings, GraphicsSettings, WindowEvent, Gaussian,
+    RIGHT_VEC, UP_VEC
+};
+use egui::{Context, Slider, TopBottomPanel};
+
+use lin_alg::f32::{Quaternion, Vec3};
+
+type Color = (f32, f32, f32);
+
+const WINDOW_TITLE: &str = "Causal gravity model";
+const WINDOW_SIZE_X: f32 = 1_600.;
+const WINDOW_SIZE_Y: f32 = 1_000.;
+const BACKGROUND_COLOR: Color = (0.5, 0.5, 0.5);
+
+const RENDER_DIST: f32 = 200.;
+
+pub const BODY_SPHERE_SIZE: f32 = 0.1;
+pub const BODY_SHINYNESS: f32 = 2.;
+pub const BODY_COLOR: Color = (0., 1.0, 0.5);
+
+/// This runs whenever an event (e.g. keyboard, mouse etc) occurs, and provides information on the event.
+fn event_dev_handler(
+    state_: &mut State,
+    event: DeviceEvent,
+    scene: &mut Scene,
+    _engine_inputs: bool,
+    _dt: f32,
+) -> EngineUpdates {
+    match event {
+        DeviceEvent::MouseMotion { delta } => {
+            // let (dx, dy) = delta; // Relative cursor movement
+            // println!("Relative cursor position change: dx: {}, dy: {}", dx, dy);
+        }
+        DeviceEvent::Button { button, state } => {}
+        _ => (),
+    }
+    EngineUpdates::default()
+}
+
+fn event_win_handler(
+    state: &mut State,
+    event: WindowEvent,
+    _scene: &mut Scene,
+    _dt: f32,
+) -> EngineUpdates {
+    match event {
+        WindowEvent::CursorMoved {device_id, position} => {}
+        _ => (),
+    }
+    EngineUpdates::default()
+}
+
+/// This runs each frame.
+fn render_handler(_state: &mut State, _scene: &mut Scene, _dt: f32) -> EngineUpdates {
+    EngineUpdates::default()
+}
+
+const SLIDER_WIDTH: f32 = 460.;
+const SLIDER_WIDTH_ORIENTATION: f32 = 100.;
+
+pub const ROW_SPACING: f32 = 22.;
+pub const COL_SPACING: f32 = 30.;
+
+/// This function draws the (immediate-mode) GUI.
+/// [UI items](https://docs.rs/egui/latest/egui/struct.Ui.html#method.heading)
+pub fn ui_handler(state: &mut State, ctx: &Context, scene: &mut Scene) -> EngineUpdates {
+    let mut engine_updates = EngineUpdates::default();
+
+    TopBottomPanel::top("0").show(ctx, |ui| {
+        ui.spacing_mut().slider_width = SLIDER_WIDTH;
+
+        ui.horizontal(|ui| {
+            ui.add_space(COL_SPACING);
+            ui.label("Time:");
+        });
+
+        ui.add_space(ROW_SPACING / 2.);
+    });
+
+    engine_updates
+}
+
+
+fn draw_entities(entities: &mut Vec<Entity>, coordinates: &Vec<Coordinate>) {
+    *entities = Vec::new();
+
+    let mut index = 0;
+    for coordinate in coordinates {
+        // Alternate way to construct: use its `Default` impl, overriding fields as required.
+        entities.push(
+            // manually set the `scale_partial` field with a `Vec3` if using non-uniform scaling. 
+            Entity::new(
+                index, // Index of the mesh.
+                Vec3::new(
+                    (coordinate.x as f32) / 1.,
+                    (coordinate.y as f32) / 1.,
+                    (coordinate.z as f32) / 1.
+                ),
+                Quaternion::default(),
+                1.,
+                (0.0, 0.0, 10.0),
+                BODY_SHINYNESS,
+            )
+        );
+        index += 1;
+    }
+}
+
+/// Entry point to our render and event loop.
+fn render(state: State, coordinates: &Vec<Coordinate>) {
+    let mut scene = Scene {
+        meshes: vec![
+            Mesh::new_sphere(1., 3)
+        ],
+        entities: Vec::new(), // updated below.
+        gaussians: vec![
+            Gaussian {
+                center: Vec3::new_zero(),
+                amplitude: 1.,
+                width: 3.,
+                color: [1., 0., 0.5, 1.],
+            }
+        ],
+        camera: Camera {
+            fov_y: TAU / 8.,
+            position: Vec3::new(0., 10., -20.),
+            far: RENDER_DIST,
+            orientation: Quaternion::from_axis_angle(RIGHT_VEC, TAU / 16.),
+            ..Default::default()
+        },
+        lighting: Lighting {
+            ambient_color: [-1., 1., 1., 0.5],
+            ambient_intensity: 0.03,
+            point_lights: vec![
+                // Light from above and to a side.
+                PointLight {
+                    type_: LightType::Omnidirectional,
+                    position: Vec3::new(30., 50., 30.),
+                    diffuse_color: [0.3, 0.4, 0.5, 1.],
+                    specular_color: [0.3, 0.4, 0.5, 1.],
+                    diffuse_intensity: 8_000.,
+                    specular_intensity: 30_000.,
+                },
+            ],
+        },
+        input_settings: InputSettings {
+            control_scheme: ControlScheme::FreeCamera,
+            ..Default::default()
+        },
+        background_color: BACKGROUND_COLOR,
+        window_size: (WINDOW_SIZE_X, WINDOW_SIZE_Y),
+        window_title: WINDOW_TITLE.to_owned(),
+    };
+
+    let ui_settings = UiSettings {
+        layout: UiLayout::Top,
+        icon_path: Some("./resources/icon.png".to_owned()),
+    };
+
+    // Initialize entities.
+    // if !state.snapshots.is_empty() {
+    //     draw_entities(
+    //         &mut scene.entities,
+    //         &state.snapshots[state.ui.snapshot_selected],
+    //     )
+    // }
+
+    // Initialize entities.
+    draw_entities(&mut scene.entities, coordinates);
+    
+
+    // This starts the main event loop; program intereactions from here on out will 
+    // be handled by one of the `_handler` callbacks defined above.
+    graphics::run(
+        state,
+        scene,
+        ui_settings,
+        GraphicsSettings::default(),
+        render_handler,
+        event_dev_handler,
+        event_win_handler,
+        ui_handler,
+    );
+}
+
+struct State {} // Set this up however you'd like.
+
+impl State {}
+
 #[derive(Debug)]
 struct Particle {
     id: i32,
@@ -59,10 +254,11 @@ fn main() {
     let mut particles: Vec<Particle> = Vec::new();
     let mut coordinates: Vec<Coordinate> = Vec::new();
     let mut partical_id = 0;
-    for x in 0..10 {
-        for y in 0..10 {
-            for z in 0..10 {
-                if (x ^ y ^ z) != 0 && !(x > 0 && y > 0 && z > 0) {  // todo don't think this is right. Re-think
+    let max: i32 = 10;
+    for x in 0..max {
+        for y in 0..max {
+            for z in 0..max {
+                if x == 0 || x == (max-1) || y == 0 || y == (max-1) || z == 0 || z == (max-1) {
                     let coordinate = Coordinate::new(x, y, z, partical_id);
                     particles.push(Particle::new(partical_id, 37982, 1));
                     coordinates.push(coordinate);
@@ -76,6 +272,9 @@ fn main() {
         particles.push(Particle::new(i, 37982, 1));
         coordinates.push(coordinate);
     }
+    
+    let state: State = State{};
+    render(state, &coordinates);
     
     tick(particles, coordinates);
 }
